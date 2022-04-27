f25de6e5f1753003f25d526dd39faed73a06471f
royal-dargon <2312749625@qq.com>
2022-04-28 00:44:56 +0800

    update:updata some api

--- app/models.py
+++ app/models.py
@@ -12,8 +12,6 @@ class Project(db.Model):
     name = db.Column(db.String(25))
     manager_id = db.Column(db.BigInteger)
     unit_id = db.Column(db.BigInteger)
-    expected_performance_id = db.Column(db.BigInteger)
-    content = db.Column(db.Text)
 
 
 class Manager(db.Model):
@@ -25,7 +23,13 @@ class Manager(db.Model):
     name = db.Column(db.String(25))
     unit = db.Column(db.String(25))
     position = db.Column(db.String(25))
-    sex = db.Column(db.SmallInteger)
+    email = db.Column(db.String(25))
+    gender = db.Column(db.SmallInteger)
+    nationality = db.Column(db.String(20))
+    birthday = db.Column(db.String(20))
+    expertise = db.Column(db.String(20))
+    tutor = db.Column(db.String(10))
+    system = db.Column(db.String(10))
 
 
 class Unit(db.Model):
@@ -35,15 +39,31 @@ class Unit(db.Model):
     __tablename__ = 'unit'
     id = db.Column(db.BigInteger, primary_key=True)
     name = db.Column(db.String(25))
+    phone = db.Column(db.String(30))
+    post = db.Column(db.String(20))
     location = db.Column(db.String(30))
 
 
-class Expected_Performance(db.Model):
-    """
-    项目预期成果
-    """
-    __tablename__ = 'expected_performance'
+class Member_info(db.Model):
+    __tablename__ = 'member_info'
     id = db.Column(db.BigInteger, primary_key=True)
-    content = db.Column(db.Text)
-    expenses = db.Column(db.BigInteger)
-    deadline = db.Column(db.DateTime)
+    name = db.Column(db.String(20))
+    gender = db.Column(db.String(10))
+    birthday = db.Column(db.String(20))
+
+
+class Member2pro(db.Model):
+    __tablename__ = 'member2pro'
+    id = db.Column(db.BigInteger, primary_key=True)
+    mid = db.Column(db.BigInteger)
+    pid = db.Column(db.BigInteger)
+
+# class Expected_Performance(db.Model):
+#     """
+#     项目预期成果
+#     """
+#     __tablename__ = 'expected_performance'
+#     id = db.Column(db.BigInteger, primary_key=True)
+#     content = db.Column(db.Text)
+#     expenses = db.Column(db.BigInteger)
+#     deadline = db.Column(db.DateTime)
--- db.sql
+++ db.sql
@@ -1,6 +1,7 @@
 DROP DATABASE IF EXISTS `file2pass`;
 CREATE DATABASE `file2pass`;
 USE `file2pass`;
+
 CREATE TABLE `project` (
   `id` INT AUTO_INCREMENT COMMENT "项目编号",
   `type` VARCHAR(25) NOT NULL COMMENT "项目类型",
@@ -8,28 +9,62 @@ CREATE TABLE `project` (
   `name` VARCHAR(25) NOT NULL COMMENT "项目名称",
   `manager_id` INT NOT NULL COMMENT "项目负责人id",
   `unit_id` INT NOT NULL COMMENT "依托单位id",
-  `expected_performance_id` INT NOT NULL COMMENT "项目的预期信息id",
-  `content` TEXT NOT NULL COMMENT "项目正文",
   PRIMARY KEY(`id`)
 ) ENGINE = InnoDB DEFAULT CHARSET = UTF8MB4;
+
+INSERT INTO project values(1, "一般项目", "本项目主要是为了帮助代价合理的学习计算机", "学习法则", 1, 1);
+INSERT INTO project values(2, "大型项目", "本项目主要是为了计算机的进阶学习的方式", "进阶学习", 2, 2);
+
 CREATE TABLE `manager` (
   `id` INT AUTO_INCREMENT COMMENT "项目负责人id",
   `name` VARCHAR(10) NOT NULL COMMENT "项目负责人姓名",
   `unit` VARCHAR(25) NOT NULL COMMENT "项目负责人目前所在单位",
   `position` VARCHAR(25) NOT NULL COMMENT "项目负责人目前职位",
-  `sex` TINYINT(1) NOT NULL COMMENT "性别 0-男性 1-女性",
+  `email` varchar(25) not null COMMENT "项目负责人的邮箱",
+  `gender` varchar(10) NOT NULL COMMENT "性别 0-男性 1-女性",
+  `nationality` varchar(10) not null COMMENT "项目负责人的民族",
+  `birthday` varchar(30) not null COMMENT "项目负责人的生日",
+  `expertise` varchar(20) not null COMMENT "项目负责人的专长",
+  `tutor` varchar(10) not null COMMENT "项目负责人担任的导师类型",
+  `system` varchar(15) not null COMMENT "项目负责人所属的系统",
   PRIMARY KEY(`id`)
 ) ENGINE = InnoDB DEFAULT CHARSET = UTF8MB4;
+
+INSERT INTO manager values(1, "张旷", "华中师范大学", "主任", "2312", "男", "汉", "2002-5-1", "计算机", "硕士生导师", "教育系统");
+INSERT INTO manager values(2, "张广", "华中科技大学", "主任", "2312", "男", "汉", "2002-5-1", "计算机", "硕士生导师", "教育系统");
+
 CREATE TABLE `unit` (
   `id` INT AUTO_INCREMENT COMMENT "依托单位id",
   `name` VARCHAR(10) NOT NULL COMMENT "依托单位名称",
-  `location` VARCHAR(30) NOT NULL COMMENT "依托单位所属地",
+  `phone` VARCHAR(30) NOT NULL COMMENT "依托单位的电话信息",
+  `post` varchar(10) not null COMMENT "依托单位的邮政编码",
+  `location` varchar(30) not null COMMENT "依托单位的地址",
   PRIMARY KEY(`id`)
 ) ENGINE = InnoDB DEFAULT CHARSET = UTF8MB4;
-CREATE TABLE `expected_performance` (
-  `id` INT AUTO_INCREMENT COMMENT "项目预期成果id",
-  `content` TEXT NOT NULL COMMENT "项目预期成果",
-  `expenses` DOUBLE NOT NULL COMMENT "项目经费",
-  `deadline` DATETIME NOT NULL COMMENT "项目预期的完成时间",
-  PRIMARY KEY(`id`)
+
+INSERT INTO unit values(1, "华中师范大学", "10068", "2245", "洪山去");
+INSERT INTO unit values(2, "华中科技大学", "10068", "2245", "洪山去");
+
+create table  `member_info` (
+   `id` int AUTO_INCREMENT  COMMENT "成员id",
+   `name` varchar(10) not null COMMENT "成员姓名",
+   `gender` varchar(5) not null COMMENT "成员性别",
+   `birthday` varchar(20) not null COMMENT "成员生日",
+   PRIMARY KEY(`id`)
 ) ENGINE = InnoDB DEFAULT CHARSET = UTF8MB4;
+
+INSERT INTO member_info values(1, "崔劲", "女", "2003-4-5");
+INSERT INTO member_info values(2, "卢杰", "男", "2003-4-5");
+INSERT INTO member_info values(3, "涛涛", "女", "2003-4-5");
+
+create table `member2pro` (
+    `id`  int AUTO_INCREMENT,
+    `mid` int COMMENT "成员的id",
+    `pid` int COMMENT "项目的id",
+    PRIMARY KEY(`id`)
+) ENGINE = InnoDB DEFAULT CHARSET = UTF8MB4;
+
+INSERT INTO member2pro values(1,1,1);
+INSERT INTO member2pro values(2,1,2);
+INSERT INTO member2pro values(3,2,1);
+INSERT INTO member2pro values(4,3,1);

28427a85efeef0043b5cfbd611e0138708e4148d
royal-dargon <2312749625@qq.com>
2022-05-03 02:14:46 +0800

    add:add a new api

--- app/api/parseFile.py
+++ app/api/parseFile.py
@@ -3,9 +3,10 @@ from flask import jsonify, flash, request
 import os
 from werkzeug.utils import secure_filename
 import pdfplumber
-from app.models import Project, Manager, Member_info, Unit, Member2pro
+from app.models import Project, Manager, Member_info, Unit, Member2pro, TextInfo
 from app import db
 from ..utils import pure_format
+from docx import Document
 
 ALLOWED_EXTENSIONS = {'doc', 'docx', 'pdf'}
 UPLOAD_FOLDER = 'C:/Users/hp/File2PassBackend/app/static'
@@ -36,7 +37,11 @@ def upload_file():
             filename = secure_filename(file.filename)
             file.save(rePlace(os.path.join(UPLOAD_FOLDER, filename)))
 
-            parse_table(filename)
+            ok = filename.endswith(".pdf")
+            if ok is True:
+                parse_table(filename)
+            else:
+                getDataFromTable(filename)
 
             return jsonify({
                 'msg': "上传文件成功"
@@ -52,26 +57,42 @@ def parse_table(filename):
                 row = list(filter(None, row))
                 row = list(map(pure_format, row))
                 data.append(row)
-
+        print(data)
         manager = Manager()
         manager.name = data[1][1]
         manager.birthday = data[1][7]
         manager.system = data[4][3]
-        manager.expertise = data[2][5]
+        manager.expertise = data[3][5]
         manager.tutor = data[3][3]
         manager.nationality = data[1][5]
-        manager.unit = data[8][1]
-        manager.position = data[2][1]
+        manager.unit = data[9][1]
+        manager.position = data[3][1]
         manager.gender = data[1][3]
         db.session.add(manager)
         db.session.commit()
 
         unit = Unit()
-        unit.name = data[8][1]
-        unit.post = data[8][3]
-        unit.location = data[10][3]
-        unit.phone = data[10][1]
+        unit.name = data[9][1]
+        unit.post = data[9][3]
+        unit.location = data[11][3]
+        unit.phone = data[11][1]
         db.session.add(unit)
+        db.session.commit()
+
+        textInfo = TextInfo()
+        content = ""
+        for i in range(2, 4):
+            page = pdf.pages[i]
+            page_content = '\n'.join(page.extract_text().split('\n')[:-1])
+            content = content + page_content
+            textInfo.argument = content
+        cont = ""
+        for i in range(4, len(pdf.pages)):
+            page = pdf.pages[i]
+            page_content = '\n'.join(page.extract_text().split('\n')[:-1])
+            content = cont + page_content
+            textInfo.guarantee = cont
+        db.session.add(textInfo)
         db.session.commit()
 
         project = Project()
@@ -81,14 +102,15 @@ def parse_table(filename):
         lines = list(map(pure_format, list(text.split("\n"))))
 
         project.type = lines[2][-4:]
-        project.summary = data[18][0]
+        project.summary = data[-2][0]
         project.name = data[0][1]
         project.manager_id = manager.id
         project.unit_id = unit.id
+        project.text_id = textInfo.id
         db.session.add(project)
         db.session.commit()
 
-        for i in range(12, 17):
+        for i in range(13, 17):
             if len(data[i]) < 2:
                 break
             member = Member_info()
@@ -104,6 +126,87 @@ def parse_table(filename):
             member2pro.pid = project.id
             db.session.add(member2pro)
             db.session.commit()
+
+
+
+def getDataFromTable(path):
+    path = os.path.join(UPLOAD_FOLDER, path)
+    document = Document(path)
+    # 获取文档中的所有表格
+    all_tables = document.tables
+
+    cell_list = []
+    for table in all_tables:
+        for row in table.rows:
+            cell_lists = []
+            cell_lists.clear()
+            text = ''
+            for cell in row.cells:
+                # print("1", cell)
+                tempt = text
+                text = cell.text.replace('\n', '').replace(' ', '')
+                if tempt == text:
+                    continue
+                cell_lists.append(text)
+            # print("1", cell_lists)
+            cell_list.append(cell_lists)
+    # print(len(cell_list[20]))
+    manager = Manager()
+    manager.name = cell_list[7][1]
+    manager.birthday = cell_list[7][7]
+    manager.system = cell_list[11][3]
+    manager.expertise = cell_list[9][5]
+    manager.tutor = cell_list[10][3]
+    manager.nationality = cell_list[7][5]
+    manager.unit = cell_list[15][1]
+    manager.position = cell_list[9][1]
+    manager.gender = cell_list[7][3]
+    db.session.add(manager)
+    db.session.commit()
+
+    unit = Unit()
+    unit.name = cell_list[15][1]
+    unit.post = cell_list[15][3]
+    unit.location = cell_list[17][3]
+    unit.phone = cell_list[17][1]
+    db.session.add(unit)
+    db.session.commit()
+
+    textInfo = TextInfo()
+    textInfo.argument = cell_list[-2][0]
+    textInfo.guarantee = cell_list[-1][0]
+    db.session.add(textInfo)
+    db.session.commit()
+
+    project = Project()
+    project.type = cell_list[1][1]
+    project.summary = cell_list[22][0]
+    project.name = cell_list[2][1]
+    project.manager_id = manager.id
+    project.unit_id = unit.id
+    project.text_id = textInfo.id
+    db.session.add(project)
+    db.session.commit()
+
+    for i in range(19, 25):
+        if len(cell_list[i]) < 3:
+            break
+        member = Member_info()
+        member.name = cell_list[i][1]
+        member.gender = cell_list[i][2]
+        member.birthday = cell_list[i][3]
+
+        db.session.add(member)
+        db.session.commit()
+
+        member2pro = Member2pro()
+        member2pro.mid = member.id
+        member2pro.pid = project.id
+        db.session.add(member2pro)
+        db.session.commit()
+
+            # getDataFromTable(r"../static/1.docx")
+
 
 def rePlace(url):
     url = url.replace("/", "\\")
